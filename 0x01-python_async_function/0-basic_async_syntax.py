4e36e55c505acfb1652d897278989d4785f6b9e6
AMGHNOUSS <redouane.amghnouss@edu.uiz.ac.ma>
2024-10-14 23:35:59 +0100

    Update code 0-basic_async_syntax.py

--- 0x01-python_async_function/0-basic_async_syntax.py
+++ 0x01-python_async_function/0-basic_async_syntax.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python3
-""" """
+""" The basics of async"""
 
 import asyncio
 import random
 
+
 async def wait_random(number=10):
-    n = random.randint(0, number)
+    n = random.uniform(0, number)
     await asyncio.sleep(n)
     return (n)
